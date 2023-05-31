3185055a98efe7890783d3fa4bc13da5009f6537
Pengcheng Shi <34006160+PengchengShi1220@users.noreply.github.com>
2023-05-31 15:12:31 +0800

    Update nnUNetTrainerV2_nextou_noMirroring.py

--- network_training/nnUNetTrainerV2_nextou_noMirroring.py
+++ network_training/nnUNetTrainerV2_nextou_noMirroring.py
@@ -1,4 +1,4 @@
-# 2022.03.16-Changed for training NexToU without Mirroring
+# 2023.03.16-Changed for training NexToU without Mirroring
 #            Harbin Institute of Technology (Shenzhen), <pcshi@stu.hit.edu.cn>
 
 #    Copyright 2020 Division of Medical Image Computing, German Cancer Research Center (DKFZ), Heidelberg, Germany
